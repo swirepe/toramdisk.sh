f0779d6bc3c3739b661360f428664dc3605ac34c
Peter Swire <swirepe@gmail.com>
2012-11-20 16:30:17 -0500

    added in a size option

--- toramdisk.sh
+++ toramdisk.sh
@@ -6,14 +6,17 @@ DIRECTORY_TO_COPY=''
 RAMDISK_ROOT="/tmp/ramdisk"
 
 
-
 # get the arguments
 if [ $# -le 0 ]
 then
-    echo  "toramdisk - put a directory into memory"
+    echo  "toramdisk - Put a directory into memory."
     echo  "Positonal arguments:"
     echo  "    directory               Put this directory into memory."
     echo  "    ramdisk_root (optional) Location to put it in memory. Default: $RAMDISK_ROOT"
+    echo  "    size         (optional) The amount of ram to use."
+    echo  "                            Defaults to the size of the directory to put in memory."
+    echo  "                            WARNING: will fail if too small"
+    echo  "Output: the path to the directory now in memory."                                   
     exit 0    
 fi
 
@@ -22,22 +25,33 @@ if [ -n "$1" ]
 then
     DIRECTORY_TO_COPY="$1"
 else
-    echo "Error: directory not specified."
+    echo "Error: directory not specified." > /dev/stderr
     exit 1
 fi
 
+# make sure that what we are to copy is a directory and it exists
+if [ ! -d "$DIRECTORY_TO_COPY" ]
+then
+    echo "Error: non-directory specified.  $DIRECTORY_TO_COPY" > /dev/stderr
+    exit 1
+fi
+
+# get the ramdisk root, if we need to
 if [ -n "$2" ]
 then
     RAMDISK_ROOT="$2"
 fi
 
-
-# make sure that what we are to copy is a directory and it exists
-if [ ! -d "$DIRECTORY_TO_COPY" ]
+# get the size, if we need to
+if [ -n "$3" ]
 then
-    echo "Error: non-directory specified.  $DIRECTORY_TO_COPY"
-    exit 1
+    SIZE="$3"
+else
+    SIZE=$(du -c -h $DIRECTORY_TO_COPY | cut -f 1 | tail -n 1)
 fi
+
+
+
 
 
 # make the root
@@ -51,7 +65,6 @@ NAME=$(readlink -f $DIRECTORY_TO_COPY | xargs basename)
 sudo mkdir -p "$RAMDISK_ROOT/$NAME"
 
 # mount the directory
-SIZE=$(du -c -h $DIRECTORY_TO_COPY | cut -f 1 | tail -n 1)
 sudo mount -t tmpfs -o size="$SIZE" tmpfs "$RAMDISK_ROOT/$NAME"
 
 # copy the files
